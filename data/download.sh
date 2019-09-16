a0914fcbd9c00a970b8177a03c0802857db6b7cf
khanhptnk <nguyenxuankhanhm@gmail.com>
2019-09-16 11:45:58 -0400

    update

--- data/download.sh
+++ data/download.sh
@@ -10,9 +10,9 @@ yes | rm -rf Matterport3DSimulator
 wget -O hanna_data.zip https://www.dropbox.com/s/qfahoe22vh1cdhn/hanna_data.zip?dl=1
 unzip hanna_data.zip
 
-#mkdir img_features
-#cd img_features
-#wget -O ResNet-152-imagenet.zip https://www.dropbox.com/s/715bbj8yjz32ekf/ResNet-152-imagenet.zip?dl=1
-#unzip ResNet-152-imagenet.zip
+mkdir img_features
+cd img_features
+wget -O ResNet-152-imagenet.zip https://www.dropbox.com/s/715bbj8yjz32ekf/ResNet-152-imagenet.zip?dl=1
+unzip ResNet-152-imagenet.zip
 
 
